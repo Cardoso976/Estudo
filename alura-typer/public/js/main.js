829850432ff3b8e03b2e72165d5ed442b353b763
Rodrigo Garcia Cardoso <rodrigo_cardoso97@hotmail.com>
2017-10-17 10:13:12 -0200

    Modernize css

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -5,6 +5,7 @@ $(function(){
 	atualizaTamanhoFrase();
 	inicializaContadores();
 	inicializaCronometro();
+	inicializaMarcadores();
 	$("#botao-reiniciar").click(reiniciaJogo);
 });
 
@@ -34,8 +35,27 @@ function inicializaCronometro() {
 			if(tempoRestante < 1){
 				campo.attr("disabled", true);
 				clearInterval(cronometroId);
+				campo.toggleClass("campo-desativado");
 			}
 		},1000);
+	}); 
+}
+
+function inicializaMarcadores() {
+	var frase = $(".frase").text();
+	campo.on("input", function(){
+		var digitado = campo.val();
+		var comparavel = frase.substring(0, digitado.length);
+		console.log("Digitado:" + digitado);
+		console.log("Frase C.:" + comparavel);
+
+		if(digitado == comparavel){
+			campo.addClass("borda-verde");
+			campo.removeClass("borda-vermelha");
+		}else {
+			campo.addClass("borda-vermelha");
+			campo.removeClass("borda-verde");
+		}
 	}); 
 }
 
@@ -46,4 +66,7 @@ function reiniciaJogo () {
 	$("#contador-caracteres").text("0");
 	$("#tempo-digitacao").text(tempoInicial);
 	inicializaCronometro();
+	campo.toggleClass("campo-desativado"); 
+	campo.removeClass("borda-verde");
+	campo.removeClass("borda-vermelha");
 }
