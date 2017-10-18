55e8620d4f89a9d23b7f1e6d57c09302aec60622
Rodrigo Garcia Cardoso <rodrigo_cardoso97@hotmail.com>
2017-10-18 14:28:26 -0200

    Carrinho de compra

--- fatec-shop/js/site.js
+++ fatec-shop/js/site.js
@@ -1,3 +1,24 @@
 $("#logar").click(function(){	
-	$("#myModal").show();		
+	$("#login").modal('toggle(slow/400/fast)');		
 });
+
+$("#registro").click(function(){	
+	$("#registrar").modal('toggle(slow/400/fast)');		
+});
+
+$("#confimar-login").click(function(){
+	
+	var senha = $("#psw").val();
+	var usuario = $("#usrname").val();
+
+	bootbox.alert("Olá "+usuario);
+	$("#login").modal('toggle');	
+});
+
+(function(){
+ 
+  $("#cart").on("click", function() {
+    $(".shopping-cart").fadeToggle( "fast");
+  });
+  
+})();
