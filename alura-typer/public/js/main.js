74cacb3e905e41df570eb9ea384c235ebb179148
Rodrigo Garcia Cardoso <rodrigo_cardoso97@hotmail.com>
2017-10-20 17:18:08 -0200

    Separacao .js

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -6,8 +6,7 @@ $(function(){
 	inicializaContadores();
 	inicializaCronometro();
 	inicializaMarcadores();
-	$("#botao-reiniciar").click(reiniciaJogo);
-	$(".botao-remover").click(removeLinha);
+	$("#botao-reiniciar").click(reiniciaJogo);	
 });
 
 function atualizaTamanhoFrase() {
@@ -62,42 +61,6 @@ function inicializaMarcadores() {
 		}
 	}); 
 }
-
-function inserePlacar(){
-	var corpoTabela = $(".placar").find("tbody");
-	var usuario = "Rodrigo";
-	var numPalavras = $("#contador-palavras").text(); 
-
-	var linha = novaLinha(usuario, numPalavras);
-	linha.find(".botao-remover").click(removeLinha);
-	corpoTabela.prepend(linha);				
-}
-
-function novaLinha(usuario, numPalavras){
-	var linha = $("<tr>");
-	var colunaUsuario = $("<td>").text(usuario);
-	var colunaPalavras = $("<td>").text(numPalavras);
-	var colunaRemover = $("<td>");
-
-	var link = $("<a>").addClass("botao-remover").attr("href", "#");
-	var icone = $("<i>").addClass("small").addClass('material-icons').text('delete');
-
-	link.append(icone);
-
-	colunaRemover.append(link);
-
-	linha.append(colunaUsuario);
-	linha.append(colunaPalavras);
-	linha.append(colunaRemover);
-
-	return linha;
-}
-
-function removeLinha(){
-	event.preventDefault();
-	$(this).parent().parent().remove();
-}
-
 
 function reiniciaJogo () {	
 	campo.attr("disabled", false);
