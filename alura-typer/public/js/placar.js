7fa0475e2fda26ec8b545537e06f5b2f133017ee
Rodrigo Garcia Cardoso <rodrigo_cardoso97@hotmail.com>
2017-10-25 11:17:59 -0200

    Animacao Placar

--- alura-typer/public/js/placar.js
+++ alura-typer/public/js/placar.js
@@ -1,42 +1,56 @@
-$(function(){
-	$(".botao-remover").click(removeLinha);
-	$("#botao-placar").click(mostraPlacar);
-});
-function inserePlacar(){
-	var corpoTabela = $(".placar").find("tbody");
-	var usuario = "Rodrigo";
-	var numPalavras = $("#contador-palavras").text(); 
+$("#botao-placar").click(mostraPlacar);
 
-	var linha = novaLinha(usuario, numPalavras);
-	linha.find(".botao-remover").click(removeLinha);
-	corpoTabela.prepend(linha);				
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Rodrigo"
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+    $(".placar").slideDown(500);
+    scrollPlacar();
 }
 
-function novaLinha(usuario, numPalavras){
-	var linha = $("<tr>");
-	var colunaUsuario = $("<td>").text(usuario);
-	var colunaPalavras = $("<td>").text(numPalavras);
-	var colunaRemover = $("<td>");
-
-	var link = $("<a>").addClass("botao-remover").attr("href", "#");
-	var icone = $("<i>").addClass("small").addClass('material-icons').text('delete');
-
-	link.append(icone);
-
-	colunaRemover.append(link);
-
-	linha.append(colunaUsuario);
-	linha.append(colunaPalavras);
-	linha.append(colunaRemover);
-
-	return linha;
+function scrollPlacar() {
+    var posicaoPlacar = $(".placar").offset().top;
+    $("body").animate(
+    {
+        scrollTop: posicaoPlacar + "px"
+    }, 1000);
 }
 
-function removeLinha(){
-	event.preventDefault();
-	$(this).parent().parent().remove();
+function novaLinha(usuario, palavras) {
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
 }
 
-function mostraPlacar(){
-	$(".placar").slideDown(2000);
+function removeLinha() {
+    event.preventDefault();
+    var linha = $(this).parent().parent();
+
+    linha.fadeOut(1000);
+    setTimeout(function() {
+        linha.remove();
+    }, 1000);
+}
+
+function mostraPlacar() {
+    $(".placar").stop().slideToggle(600);
 }
